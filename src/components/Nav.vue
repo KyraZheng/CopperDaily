2dfb0d3eaa6b8f594bc1c25352a45544ba86dc74
ZQakane <zhenq021@gmail.com>
2020-05-07 22:00:10 +0800

    创建<Icon/>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="nav">
-  <router-link to="/labels">标签</router-link>|
-  <router-link to="/copper">记一笔</router-link> |
-  <router-link to="/statistics">统计</router-link>
+  <router-link to="/labels">
+    <Icon name="labels"/>
+    标签
+  </router-link>|
+  <router-link to="/copper">
+    <Icon name="copper"/>
+    记一笔
+  </router-link> |
+  <router-link to="/statistics">
+    <Icon name="statistics"/>
+    统计
+  </router-link>
   </div>
 </template>
 
 <script lang="ts">
-  import x from '@/assets/icons/labels.svg'
   export default {
     name: 'Nav'
-  }
+  };
 </script>
 
 <style scoped lang="scss">

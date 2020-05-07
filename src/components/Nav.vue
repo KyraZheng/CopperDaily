64ea451a1366951d29dde131b246fda2022b3c2a
ZQakane <zhenq021@gmail.com>
2020-05-07 22:27:35 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="nav">
-  <router-link to="/labels">
+  <nav>
+  <router-link to="/labels" class="item">
     <Icon name="labels"/>
     标签
-  </router-link>|
-  <router-link to="/copper">
+  </router-link>
+  <router-link to="/copper" class="item">
     <Icon name="copper"/>
     记一笔
-  </router-link> |
-  <router-link to="/statistics">
+  </router-link>
+  <router-link to="/statistics" class="item">
     <Icon name="statistics"/>
     统计
   </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -22,8 +22,25 @@
 </script>
 
 <style scoped lang="scss">
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,1);
+  flex-direction: row;
+  padding: 4px;
+
+> .item {
+  font-size: 14px;
+  padding: 2px 0;
+  width: 33.333333%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+.icon{
+  width: 32px;
+  height: 32px;
+}
+}
 
 }
 </style>

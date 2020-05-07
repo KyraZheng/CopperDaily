76af1e7f8f76284c6b9a6417490e0bb071643bdc
ZQakane <zhenq021@gmail.com>
2020-05-07 17:55:41 +0800

    '添加svg'

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="nav">
-  <router-link to="/labels">明细</router-link>|
+  <router-link to="/labels">标签</router-link>|
   <router-link to="/copper">记一笔</router-link> |
   <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+  import x from '@/assets/icons/labels.svg'
   export default {
     name: 'Nav'
   }

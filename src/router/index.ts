7d54d6368e3876a35d682be8638203a32cb45096
ZQakane <zhenq021@gmail.com>
2020-05-23 15:50:12 +0800

    将 tag 的 string 改为 { id, name }

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,8 @@ import Home from '../views/Home.vue';
 import Copper from '@/views/Copper.vue';
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
-import NotFound from '@/views/NotFound.vue'
+import NotFound from '@/views/NotFound.vue';
+import EditLabel from '@/views/EditLabel.vue';
 
 Vue.use(VueRouter);
 
@@ -14,20 +15,24 @@ const routes: Array<RouteConfig> = [
     redirect:'/copper'
   },
   {
-    path:'/copper',
-    component:Copper
+    path: '/copper',
+    component: Copper
   },
   {
-    path:'/labels',
-    component:Labels
+    path: '/labels',
+    component: Labels
   },
   {
-    path:'/statistics',
-    component:Statistics
+    path: '/statistics',
+    component: Statistics
   },
   {
-    path:'*',
-    component:NotFound
+    path: '/labels/edit',
+    component: EditLabel
+  },
+  {
+    path: '*',
+    component: NotFound
   }
 ];
 

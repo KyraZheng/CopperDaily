4d12a13d00dbc89c01e5ec64d6abfd8e1ebdea49
ZQakane <zhenq021@gmail.com>
2020-05-28 13:36:37 +0800

    解决了 Tags 的bug

--- src/views/Copper.vue
+++ src/views/Copper.vue
@@ -5,7 +5,7 @@
     <div class="notes">
       <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <Tags/>
   </Layout>
 </template>
 
@@ -23,8 +23,6 @@
   })
 
   export default class Copper extends Vue {
-
-    tags = store.tagList;
     recordList = store.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
@@ -32,10 +30,6 @@
 
     onUpdateNotes(value: string) {
       this.record.notes = value;
-    }
-
-    onUpdateTags(value: string[]) {
-      this.record.tags = value;
     }
 
     onUpdateAmount(value: string) {

c0348dbe8dc5f027a6ddecf6d2eb02a89313543f
ZQakane <zhenq021@gmail.com>
2020-06-02 22:35:07 +0800

    加入二维码

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/CopperDaily-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
-    const dir = path.resolve(__dirname,'src/assets/icons')
+    const dir = path.resolve(__dirname, 'src/assets/icons')
 
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 包含icons目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
